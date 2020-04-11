0c90e460b98b7305a66ecddd090812ca51ae6337
ggggmpfi <56532905+ggggmpfi@users.noreply.github.com>
2020-04-11 08:40:26 +0800

    Create ChangMAC.sh

---
+++ ChangMAC.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+ifconfig wlp3s0 down;
+ifconfig wlp3s0 hw ether 2C:B2:1A:ED:1E:51;
+ifconfig wlp3s0 up;
+#备用MAC地址
+#FC:D7:33:2C:54:27		TP-LINK TECHNOLOGIES CO.,LTD.（路由器）
+#4C:B0:08:36:B8:87		Shenzhen Gwelltimes Technology Co.,Ltd（监控摄像头）
+#54:A7:03:3F:78:D5		TP-LINK TECHNOLOGIES CO.,LTD.(扩展器）
+#48:3F:E9:0B:02:A6		HUAWEI TECHNOLOGIES CO.,LTD（手机）
+#70:EC:E4:C3:4D:9E		Apple, Inc.(手机）
+#F4:F5:DB:2D:3F:CB
+#14:11:5D:91:39:35		Skyworth Digital Technology(Shenzhen) Co.,Ltd（机顶盒）
+#2C:B2:1A:ED:1E:14		Phicomm (Shanghai) Co., Ltd.（路由器）
+#14:4D:67:7F:0B:67		Zioncom Electronics (Shenzhen) Ltd.(扩展器）
+#5C:66:6C:43:70:A2		GUANGDONG OPPO MOBILE TELECOMMUNICATIONS CORP.,LTD（手机）
+#E0:13:B5:D8:C2:E0		vivo Mobile Communication Co., Ltd.(手机）
+#B4:A5:AC:14:E1:26		GUANGDONG OPPO MOBILE TELECOMMUNICATIONS CORP.,LTD（手机）
+#B4:86:55:79:17:23		HUAWEI TECHNOLOGIES CO.,LTD
+#B4:A5:AC:37:7D:CA		GUANGDONG OPPO MOBILE TELECOMMUNICATIONS CORP.,LTD（手机）
+#C4:9F:4C:CC:B0:9C		HUAWEI TECHNOLOGIES CO.,LTD（手机）
+#D4:3B:04:FA:E4:4C		Intel Corporate（电脑）
+#DC:31:D1:D9:CC:D4		vivo Mobile Communication Co., Ltd.（手机）
+#54:19:C8:FC:B7:46		vivo Mobile Communication Co., Ltd.（手机）
+#44:9E:F9:31:8A:58		vivo Mobile Communication Co., Ltd.（手机）
+#D4:61:DA:89:48:8A		Apple, Inc.（平板）
+#18:D2:76:C1:C0:64		HUAWEI TECHNOLOGIES CO.,LTD（手机）
+#4C:18:9A:60:E2:F0		GUANGDONG OPPO MOBILE TELECOMMUNICATIONS CORP.,LTD
+#30:21:8C:08:51:3B		蓝牙音箱
+#54:19:C8:FC:B7:46
+#48:7D:2E:C5:0C:4C		TP-LINK_0C4D
+#---------------------------------------------------------------------#
+#D8:C8:E9:80:8A:70		PHICOMM_68
