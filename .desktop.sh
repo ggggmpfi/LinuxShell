23e3a0f7d2c67d6e92a5faba04119062574328a8
ggggmpfi <56532905+ggggmpfi@users.noreply.github.com>
2020-04-16 14:18:54 +0800

    Add files via upload

---
+++ .desktop.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#重装桌面后的卸载工作
+#libreoffice套件
+sudo apt remove libreoffice-pdfimport
+sudo apt remove libreoffice-writer
+sudo apt remove libreoffice-calc
+sudo apt remove libreoffice-draw
+#游戏
+sudo apt remove gnome-mahjongg
+sudo apt remove gnome-mines
+sudo apt remove gnome-sudoku
+sudo apt remove aisleriot
+#系统监测monitor
+sudo apt remove gnome-system-monitor
+#日历
+sudo apt remove gnome-calendar
+#自拍软件
+sudo apt remove cheese
+#videos
+sudo apt remove totem
+#TODO
+sudo apt remove gnome-todo
+#Rhythmbox播放器
+sudo apt remove rhythmbox
+#transmission(种子下载器）
+sudo apt remove transmission-gtk
+#Remmina工具
+sudo apt remove remmina
+#screenshot
+sudo apt remove gnome-screenshot
