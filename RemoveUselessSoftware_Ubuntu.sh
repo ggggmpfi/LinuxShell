b524d57a527aedc5d3305d0f1e60b8c83f35cd3d
ggggmpfi <56532905+ggggmpfi@users.noreply.github.com>
2020-04-18 17:20:19 +0800

    Update RemoveUselessSoftware_Ubuntu.sh

--- RemoveUselessSoftware_Ubuntu.sh
+++ RemoveUselessSoftware_Ubuntu.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 #Ubuntu重装gnome桌面后的卸载工作
 #libreoffice套件
+sudo apt remove libreoffice-base-core 
+sudo apt remove libreoffice-common
 sudo apt remove libreoffice-pdfimport
 sudo apt remove libreoffice-writer
 sudo apt remove libreoffice-calc
 sudo apt remove libreoffice-draw
+sudo apt remove libreoffice-core 
+sudo apt remove libreoffice-draw 
+sudo apt remove libreoffice-gnome
+sudo apt remove libreoffice-gtk3
+sudo apt remove libreoffice-impress 
+sudo apt remove libreoffice-math
+sudo apt remove libreoffice-style-breeze
+sudo apt remove libreoffice-style-colibre
+sudo apt remove libreoffice-style-elementary
+sudo apt remove libreoffice-style-tango
 #游戏
 sudo apt remove gnome-mahjongg
 sudo apt remove gnome-mines
